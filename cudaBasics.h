903520ced944f0940cb1822fa0cf87a6d59cdc73
TheAIBot <andreasuranus@hotmail.com>
2020-08-18 00:18:51 +0200

    additional documentation, support for cuda streams and use cudaLaunch to launch a kernel

--- cudaBasics.h
+++ cudaBasics.h
@@ -99,24 +99,58 @@ namespace cudabasic
         float getElapsedMiliseconds();
     };
 
-    /// <summary>
-    /// Executes a cuda kernel
-    /// </summary>
-    /// <typeparam name="...Args"></typeparam>
-    /// <param name="kernel">Function pointer to the kernel</param>
-    /// <param name="blockDim">Block dimension for the kernel</param>
-    /// <param name="gridDim">Grid dimension for the kernel</param>
-    /// <param name="...args">Kernel arguments</param>
+	/// <summary>
+	/// Executes a cuda kernel
+	/// </summary>
+	/// <typeparam name="...Args">Kernel argument types</typeparam>
+	/// <param name="kernel">Function pointer to the kernel</param>
+	/// <param name="blockDim">Block dimension for the kernel</param>
+	/// <param name="gridDim">Grid dimension for the kernel</param>
+	/// <param name="...args">Kernel arguments</param>
     template<typename... Args>
     void executeKernel(void(*kernel)(Args...), dim3 blockDim, dim3 gridDim, Args... args)
     {
-        (*kernel)<<<gridDim, blockDim>>>(args...);
+		executeKernel<Args...>(kernel, blockDim, gridDim, 0, args...);
     }
 
+
+	/// <summary>
+	/// Executes a cuda kernel
+	/// </summary>
+	/// <typeparam name="...Args">Kernel argument types</typeparam>
+	/// <param name="kernel">Function pointer to the kernel</param>
+	/// <param name="blockDim">Block dimension for the kernel</param>
+	/// <param name="gridDim">Grid dimension for the kernel</param>
+	/// <param name="sharedMemSize">Shared memory in bytes</param>
+	/// <param name="...args">Kernel arguments</param>
 	template<typename... Args>
-	void executeKernel(void(*kernel)(Args...), dim3 blockDim, dim3 gridDim, int sharedMemSize, Args... args)
+	void executeKernel(void(*kernel)(Args...), dim3 blockDim, dim3 gridDim, size_t sharedMemSize, Args... args)
 	{
-		(*kernel) << <gridDim, blockDim, sharedMemSize >> > (args...);
+		executeKernel<Args...>(kernel, blockDim, gridDim, sharedMemSize, 0, args...);
+	}
+
+	/// <summary>
+	/// Executes a cuda kernel
+	/// </summary>
+	/// <typeparam name="...Args">Kernel argument types</typeparam>
+	/// <param name="kernel">Function pointer to the kernel</param>
+	/// <param name="blockDim">Block dimension for the kernel</param>
+	/// <param name="gridDim">Grid dimension for the kernel</param>
+	/// <param name="sharedMemSize">Shared memory in bytes</param>
+	/// <param name="stream">Cuda stream to launch the kernel on</param>
+	/// <param name="...args">Kernel arguments</param>
+	template<typename... Args>
+	void executeKernel(void(*kernel)(Args...), dim3 blockDim, dim3 gridDim, size_t sharedMemSize, cudaStream_t stream, Args... args)
+	{
+		std::array<void*, sizeof...(args)> arguments = {
+			&args...
+		};
+
+		const cudaError_t status = cudaLaunchKernel((void*)kernel, gridDim, blockDim, &arguments[0], sharedMemSize, stream);
+		if (status != cudaError::cudaSuccess)
+		{
+			throw std::runtime_error("Failed to launch kernel.");
+		}
 	}
 
     /// <summary>
