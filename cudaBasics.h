4c025a688e97f3fbf40ae15c295a385349736684
TheAIBot <andreasuranus@hotmail.com>
2020-08-25 16:47:59 +0200

    added way to execute a cooperative kernel

--- cudaBasics.h
+++ cudaBasics.h
@@ -202,6 +202,30 @@ namespace cudabasic
 		}
 	}
 
+	/// <summary>
+	/// Executes a cooperative cuda kernel
+	/// </summary>
+	/// <typeparam name="...Args">Kernel argument types</typeparam>
+	/// <param name="kernel">Function pointer to the cooperative kernel</param>
+	/// <param name="blockDim">Block dimension for the kernel</param>
+	/// <param name="gridDim">Grid dimension for the kernel</param>
+	/// <param name="sharedMemSize">Shared memory in bytes</param>
+	/// <param name="stream">Cuda stream to launch the kernel on</param>
+	/// <param name="...args">Kernel arguments</param>
+	template<typename... Args>
+	void executeCoopKernel(void(*kernel)(Args...), dim3 blockDim, dim3 gridDim, size_t sharedMemSize, cudaStream_t stream, Args... args)
+	{
+		std::array<void*, sizeof...(args)> arguments = {
+			&args...
+		};
+
+		const cudaError_t status = cudaLaunchCooperativeKernel((void*)kernel, gridDim, blockDim, &arguments[0], sharedMemSize, stream);
+		if (status != cudaError::cudaSuccess)
+		{
+			throw std::runtime_error("Failed to launch kernel.");
+		}
+	}
+
     /// <summary>
     /// Benchmarks a cuda kernel by taking the average execution time over a set amount of kernel executions
     /// </summary>
